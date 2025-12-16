736d83a2009926f251cbc1da1c78a80c833498d8
PowerMiG29 <powermig29@gmail.com>
2025-12-16 14:57:28 +0300

    logic

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var historyText: UITextView!
+    
+    @IBOutlet weak var plusButton: UIButton!
+    @IBOutlet weak var minusButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    private var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        titleLabel.text = "Значение счётчика:"
+        counterLabel.text = "0"
+        historyText.text = "История изменений:\n"
     }
-
-
+    
+    private func addHistoryMsg(_ message: String) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy HH:mm:ss"
+        let dateString = dateFormatter.string(from: Date())
+        
+        let stringToAdd = dateString + " " + message + "\n"
+        let oldText = historyText.text ?? ""
+        historyText.text = oldText + stringToAdd
+        
+        let range = NSMakeRange(historyText.text.count - 1, 1)
+        historyText.scrollRangeToVisible(range)
+    }
+    
+    private func changeCountText() {
+        counterLabel.text = String(counter)
+    }
+    
+    @IBAction func plusButtonTouch(_ sender: Any) {
+        counter += 1
+        changeCountText()
+        addHistoryMsg("Значение изменено на +1")
+    }
+    
+    @IBAction func minusButtonTouch(_ sender: Any) {
+        counter = counter <= 0 ? 0 : counter - 1
+        changeCountText()
+        addHistoryMsg("Значение изменено на -1")
+    }
+    
+    @IBAction func resetButtonTouch(_ sender: Any) {
+        counter = 0
+        changeCountText()
+        addHistoryMsg("Значение сброшено")
+    }
 }
 
